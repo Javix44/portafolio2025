1203c93efd2519dd7a460fd6288e97cb4efd92fb
Javier Martinez <javiermartinezv33@gmail.com>
2025-05-14 11:00:57 -0600

    Actualización de imágenes y corrección de mayúsculas en el script de fecha

--- resources/js/fecha.js
+++ resources/js/fecha.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const today = new Date();
     const day = today.getDate().toString().padStart(2, '0');
     const year = today.getFullYear();
-    const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-                    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    const months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+                    'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
     const month = months[today.getMonth()];
     const formattedDate = `${day} de ${month} de ${year}`;
     document.getElementById('current-date').textContent = formattedDate;
