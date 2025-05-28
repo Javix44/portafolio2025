eade0e18544aa70dcf37631f3d22d8797c271f85
Javier Martinez <javiermartinezv33@gmail.com>
2025-05-27 22:29:23 -0600

    Correcion final de animacion

--- resources/js/typewriter.js
+++ resources/js/typewriter.js
@@ -1,19 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const titulo = document.getElementById("titulo-principal");
 
-    // Añadir la clase de la animación de máquina de escribir al cargar la página
     titulo.classList.add("typewriter");
 
     let letras = titulo.textContent.trim().length;
     let i = 0;
 
-    // Usar un intervalo para simular el efecto de escritura
     let intervalo = setInterval(() => {
         i++;
         if (i >= letras) {
-            clearInterval(intervalo); // Detener el intervalo cuando se termina de escribir
-            // Iniciar una nueva animación para hacer desaparecer el cursor
-            titulo.style.animation = 'finishCursor 0.5s forwards';
+            clearInterval(intervalo);
+            titulo.style.animation = 'blink 0.7s step-end infinite';
         }
     }, 70);
 });
